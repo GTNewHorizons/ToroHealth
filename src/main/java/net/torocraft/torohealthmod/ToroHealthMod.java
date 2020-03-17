f03ce4a8b2326fcfa6bfe31119a55a167f8317aa
seitenca <alex-garcia@hotmail.es>
2020-03-17 20:39:58 +0100

    bump version

--- src/main/java/net/torocraft/torohealthmod/ToroHealthMod.java
+++ src/main/java/net/torocraft/torohealthmod/ToroHealthMod.java
@@ -13,7 +13,7 @@ import net.torocraft.torohealthmod.proxy.CommonProxy;
 public class ToroHealthMod {
 
 	public static final String MODID = "torohealthmod";
-	public static final String VERSION = "1.7.10-1.0.0";
+	public static final String VERSION = "1.7.10-1.0.1";
 	public static final String MODNAME = "ToroHealthMod";
 
 	@SidedProxy(clientSide = "net.torocraft.torohealthmod.proxy.ClientProxy", serverSide = "net.torocraft.torohealthmod.proxy.CommonProxy")
