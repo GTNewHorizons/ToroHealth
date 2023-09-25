fc81789b568641799a3dd601059138421882f130
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-25 21:48:58 +0200

    refactor mixin package

--- src/main/java/net/torocraft/torohealthmod/mixins/MixinLoadingPlugin.java
+++ src/main/java/net/torocraft/torohealthmod/mixins/MixinLoadingPlugin.java
@@ -1,4 +1,4 @@
-package net.torocraft.torohealthmod.mixin;
+package net.torocraft.torohealthmod.mixins;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/net/torocraft/torohealthmod/mixins/early/MixinEntityLivingBase.java
+++ src/main/java/net/torocraft/torohealthmod/mixins/early/MixinEntityLivingBase.java
@@ -1,20 +1,16 @@
-package net.torocraft.torohealthmod.mixin.mixins;
+package net.torocraft.torohealthmod.mixins.early;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
-import net.torocraft.torohealthmod.mixin.ducks.EntityLivingBaseDuck;
+import net.torocraft.torohealthmod.mixins.interfaces.EntityLivingBaseExt;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityLivingBase.class)
-public abstract class MixinEntityLivingBase implements EntityLivingBaseDuck {
-
-    @Shadow
-    public abstract float getHealth();
+public abstract class MixinEntityLivingBase implements EntityLivingBaseExt {
 
     @Unique
     private int torohealth$prevHealth;
