ef6b715f0f5e509ebea87c29da2e653091c4e83f
danyadev <danyadev@mail.ru>
2025-12-29 20:11:16 +0300

    use LivingHurtEvent and remove all mixin stuff

--- src/main/java/net/torocraft/torohealthmod/client/event/ToroHealthEventHandler.java
+++ src/main/java/net/torocraft/torohealthmod/client/event/ToroHealthEventHandler.java
@@ -1,10 +1,8 @@
 package net.torocraft.torohealthmod.client.event;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
-import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.torocraft.torohealthmod.client.particle.DamageParticles;
-import net.torocraft.torohealthmod.mixins.interfaces.EntityLivingBaseExt;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -12,21 +10,8 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class ToroHealthEventHandler {
 
     @SubscribeEvent(priority = EventPriority.LOW)
-    public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
-        final EntityLivingBase entity = event.entityLiving;
-        if (!entity.worldObj.isRemote) return;
-
-        final int prevHp = ((EntityLivingBaseExt) entity).torohealth$getPrevHealth();
-        final int hp = MathHelper.floor_float(entity.getHealth());
-
-        if (prevHp != hp) {
-            ((EntityLivingBaseExt) entity).torohealth$setPrevHealth(hp);
-
-            // -1 means that prevHealth was uninitialized because the Living has just spawned
-            if (prevHp != -1) {
-                DamageParticles.spawnDamageParticle(entity, prevHp - hp);
-            }
-        }
+    public void onLivingHurt(LivingHurtEvent event) {
+        DamageParticles.spawnDamageParticle(event.entityLiving, MathHelper.floor_float(event.ammount));
     }
 
 }
--- src/main/java/net/torocraft/torohealthmod/mixins/interfaces/EntityLivingBaseExt.java
+++
@@ -1,8 +0,0 @@
-package net.torocraft.torohealthmod.mixins.interfaces;
-
-public interface EntityLivingBaseExt {
-
-    int torohealth$getPrevHealth();
-
-    void torohealth$setPrevHealth(int torohealth$prevHealth);
-}
