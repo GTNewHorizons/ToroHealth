f10d300ed870e8aa6841861aa2ded7a235530d55
danyadev <danyadev@mail.ru>
2025-12-29 16:50:17 +0300

    remove useless inject

--- src/main/java/net/torocraft/torohealthmod/mixins/early/MixinEntityLivingBase.java
+++ src/main/java/net/torocraft/torohealthmod/mixins/early/MixinEntityLivingBase.java
@@ -1,15 +1,10 @@
 package net.torocraft.torohealthmod.mixins.early;
 
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.MathHelper;
 import net.torocraft.torohealthmod.mixins.interfaces.EntityLivingBaseExt;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityLivingBase implements EntityLivingBaseExt {
@@ -25,12 +20,6 @@ public abstract class MixinEntityLivingBase implements EntityLivingBaseExt {
     @Unique
     public void torohealth$setPrevHealth(int hp) {
         this.torohealth$prevHealth = hp;
-    }
-
-    @Inject(method = "readEntityFromNBT(Lnet/minecraft/nbt/NBTTagCompound;)V", at = @At("RETURN"))
-    private void torohealth$initHealthFromNBT(NBTTagCompound tag, CallbackInfo ci) {
-        EntityLivingBase self = (EntityLivingBase) (Object) this;
-        this.torohealth$prevHealth = MathHelper.floor_float(self.getHealth());
     }
 
 }
