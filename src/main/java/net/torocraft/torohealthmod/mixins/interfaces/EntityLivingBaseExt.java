845aa04e0b8d9bab5f5ccf49625839f78b625055
Wilhelm Schuster <dev@rot13.io>
2026-07-07 18:13:31 +0200

    Hide health popup on entity spawn (#7)

--- src/main/java/net/torocraft/torohealthmod/mixins/interfaces/EntityLivingBaseExt.java
+++ src/main/java/net/torocraft/torohealthmod/mixins/interfaces/EntityLivingBaseExt.java
@@ -4,5 +4,9 @@ public interface EntityLivingBaseExt {
 
     int torohealth$getPrevHealth();
 
+    int torohealth$getPrevMaxHealth();
+
     void torohealth$setPrevHealth(int torohealth$prevHealth);
+
+    void torohealth$setPrevMaxHealth(int torohealth$prevMaxHealth);
 }
